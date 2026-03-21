d30d74bab070d097faddce790fc869c61d3d5d88
dreik84 <dreik84@mail.ru>
2026-03-21 19:05:41 +0300

    Create User entity

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,24 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthDate;
+    private Integer age;
+}
