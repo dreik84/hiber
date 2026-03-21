c533248072447ec3079adf6836b0669c47563e3e
dreik84 <dreik84@mail.ru>
2026-03-21 19:05:21 +0300

    Create HibernateRunner

---
+++ src/main/java/org/example/HibernateRunner.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateRunner {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+
+            session.beginTransaction();
+
+
+            session.getTransaction().commit();
+        }
+    }
+}
