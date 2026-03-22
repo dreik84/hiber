f95167d68e663da6a00b21fd88bee21ef114d5eb
dreik84 <dreik84@mail.ru>
2026-03-22 19:01:45 +0300

    Update runner

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,19 +1,31 @@
 package org.example;
 
+import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.configure();
+//        configuration.addAnnotatedClass(User.class);
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
 
             session.beginTransaction();
 
+            session.persist(User.builder()
+                    .username("john@mail.ru")
+                    .firstname("john")
+                    .lastname("smitch")
+                    .birthDate(LocalDate.of(2000, 01, 01))
+                    .age(23)
+                    .build());
 
             session.getTransaction().commit();
         }
--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,7 +1,9 @@
 package org.example.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +16,13 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "users", schema = "public")
 public class User {
     @Id
     private String username;
     private String firstname;
     private String lastname;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
     private Integer age;
 }
