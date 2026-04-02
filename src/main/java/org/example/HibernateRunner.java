f02d824471501b125f42ef1163ec014fa8555066
dreik84 <dreik84@mail.ru>
2026-04-02 19:36:01 +0300

    Add logger

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -6,10 +6,15 @@ import org.example.entity.User;
 import org.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
 public class HibernateRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) {
 
         // TRANSIENT
@@ -21,18 +26,24 @@ public class HibernateRunner {
                 .role(Role.USER)
                 .build();
 
+        log.info("User object in transient state: {}, {}", user, user);
+
         // TRANSIENT
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
              Session session1 = sessionFactory.openSession()) {
 
             session1.beginTransaction();
 
-            session1.persist(user);
+            session1.merge(user);
             user.setFirstname("Anna");
+            log.warn("User first name was changed: {}", user);
             System.out.println(session1.isDirty());
             System.out.println(user);
+            log.debug("User: {}, session: {}", user, session1);
 
             session1.getTransaction().commit();
+        } catch (Exception e) {
+            log.error("Exception occurred: ", e);
         }
     }
 }
