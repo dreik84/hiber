e8e61d2551c0712dcc5a133ab90e70a2806494ac
dreik84 <dreik84@mail.ru>
2026-05-03 19:18:56 +0300

    Update lock

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -14,14 +14,20 @@ public class HibernateRunner {
     public static void main(String[] args) {
 
 
-
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
+             Session session = sessionFactory.openSession();
+             Session session1 = sessionFactory.openSession()) {
 
             session.beginTransaction();
+            session1.beginTransaction();
 
             User user = session.find(User.class, 2L);
+
             Payment payment = session.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+            payment.setAmount(payment.getAmount() + 10);
+
+            Payment payment1 = session1.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+            payment1.setAmount(payment1.getAmount() + 30);
 
 //            Payment payment = Payment.builder()
 //                    .amount(600)
@@ -30,9 +36,9 @@ public class HibernateRunner {
 //
 //            session.persist(payment);
 
-            System.out.println(payment);
 
             session.getTransaction().commit();
+            session1.getTransaction().commit();
         } catch (Exception e) {
             log.error("Exception occurred: ", e);
         }
